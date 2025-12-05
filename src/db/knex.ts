6fc9696c0999aac9ee6f0528849d7bd26865b6cc
nika <veronika.rovnik@posteo.de>
2025-12-05 15:08:51 +0900

    Add knex config file to be able to use single shared connection across repos

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,2 +1,18 @@
 import knexFn, { Knex } from "knex";
 
+const config: Knex.Config = {
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  }
+};
+
+export const knex = knexFn(config);
